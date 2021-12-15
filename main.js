16861baf8dc61c34334ff65b7229fead138439e1
alba02 <naomimaldo02@gmail.com>
2021-12-15 03:04:27 -0500

    Fix fetch

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ fetch('demo.json')
 })
 .then(json => console.log(json));
 */
-fetch('https://unsplash.it/600/400')
+/*fetch('https://unsplash.it/600/400')
 .then(res => res.blob())
 .then(blob =>{
     let img = document.createElement('img');
     img.src = URL.createObjectURL(blob);
     document.querySelector('body').appendChild(img);
-});
+});*/
 
 const postSection = document.querySelector('#posts');
 const postTemplate = document.querySelector
